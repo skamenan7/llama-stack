bf94583b8dad9b54b907062d360bcd522e8d4b2e
Derek Higgins <derekh@redhat.com>
2026-03-04 14:18:13 +0000

    fix(security): pin google-cloud-aiplatform to >=1.131.0 (#5037)
    https://www.cve.org/CVERecord?id=CVE-2026-2472
    
    Signed-off-by: Derek Higgins <derekh@redhat.com>

--- src/llama_stack/providers/registry/inference.py
+++ src/llama_stack/providers/registry/inference.py
@@ -201,7 +201,7 @@ def available_providers() -> list[ProviderSpec]:
             adapter_type="vertexai",
             provider_type="remote::vertexai",
             pip_packages=[
-                "google-cloud-aiplatform",
+                "google-cloud-aiplatform>=1.131.0",
             ],
             module="llama_stack.providers.remote.inference.vertexai",
             config_class="llama_stack.providers.remote.inference.vertexai.VertexAIConfig",
